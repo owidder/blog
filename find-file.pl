67920406f74ab09770747b7c479773de78e057d4
owidder <oliver.widder@gmail.com>
2013-11-10 12:14:38 +0100

    find-file scripts

---
+++ find-file.pl
@@ -0,0 +1,54 @@
+#!/opt/local/bin/perl
+
+my $pattern = $ARGV[0];
+
+@AllSha1 = `./list-all-objects.sh`;
+
+my @FoundTrees = ();
+
+for my $treeSha1 (@AllSha1) {
+	chomp $treeSha1;
+	if(!($treeSha1 =~ /pack/)) {
+		my $type = `git cat-file -t $treeSha1`;
+		chomp $type;
+
+		if($type eq "tree") {
+
+			my @Lines = `git cat-file -p $treeSha1`;
+			for my $line (@Lines) {
+				if($line =~ /$pattern/) {
+					push(@FoundTrees, $treeSha1);
+					break;
+				}
+			}
+		}
+	}
+}
+
+my @FoundCommits = ();
+
+for my $commitSha1 (@AllSha1) {
+	chomp $commitSha1;
+		if(!($commitSha1 =~ /pack/)) {
+		# printf "%s\n", $commitSha1;
+		my $type = `git cat-file -t $commitSha1`;
+		chomp $type;
+
+		if($type eq "commit") {
+
+			my @Lines = `git cat-file -p $commitSha1`;
+			for my $line (@Lines) {
+				for my $foundTreeSha1 (@FoundTrees) {
+					if($line =~ /$foundTreeSha1/) {
+						push(@FoundCommits, $commitSha1);
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+for my $foundSha1 (@FoundCommits) {
+	printf "%s\n", $foundSha1;
+}
