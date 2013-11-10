c701a1a6b1d582930c98b23058ff5c17d05bb147
owidder <oliver.widder@gmail.com>
2013-11-10 22:11:05 +0100

    no message

--- find-file.pl
+++ find-file.pl
@@ -2,24 +2,31 @@
 
 my $pattern = $ARGV[0];
 
-@AllSha1 = `./list-all-objects.sh`;
+my @AllObjects = `find .git/objects/`;
+my @AllSha1 = ();
+
+for my $object (@AllObjects) {
+	if($object =~ /([0-9a-f][0-9a-f])\/([0-9a-f]{38})/) {
+		my $sha1 = $1 . $2; 
+		push(@AllSha1, $sha1);
+	}
+}
 
 my @FoundTrees = ();
 
 for my $treeSha1 (@AllSha1) {
 	chomp $treeSha1;
-	if(!($treeSha1 =~ /pack/)) {
-		my $type = `git cat-file -t $treeSha1`;
-		chomp $type;
+	my $type = `git cat-file -t $treeSha1`;
+	chomp $type;
 
-		if($type eq "tree") {
+	if($type eq "tree") {
 
-			my @Lines = `git cat-file -p $treeSha1`;
-			for my $line (@Lines) {
-				if($line =~ /$pattern/) {
-					push(@FoundTrees, $treeSha1);
-					break;
-				}
+		my @Lines = `git cat-file -p $treeSha1`;
+		for my $line (@Lines) {
+			if($line =~ /$pattern/) {
+				printf "found tree: %s\n", $treeSha1;
+				push(@FoundTrees, $treeSha1);
+				break;
 			}
 		}
 	}
@@ -40,6 +47,7 @@ for my $commitSha1 (@AllSha1) {
 			for my $line (@Lines) {
 				for my $foundTreeSha1 (@FoundTrees) {
 					if($line =~ /$foundTreeSha1/) {
+						printf "found commit: %s\n", $commitSha1;
 						push(@FoundCommits, $commitSha1);
 						break;
 					}
